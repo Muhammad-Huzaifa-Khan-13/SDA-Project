6bc70977554d3dad4af3c6c1ad512e85d1d637d9
griraj1238@gmail.com <i230635@isb.nu.edu.pk>
2025-11-22 00:07:26 +0500

    Added files required for database

---
+++ backend/db/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package backend.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/eternex"; 
+    private static final String USER = "root"; 
+    private static final String PASSWORD = ""; 
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (Exception e) {
+            System.out.println("Database Connection Failed: " + e.getMessage());
+            return null;
+        }
+    }
+}
